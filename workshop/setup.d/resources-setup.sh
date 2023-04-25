9cf0b7d97d44c72844798d258dcf0613d06033ca
Timo Salm <tsalm@vmware.com>
2023-04-25 23:16:50 +0200

    Initial commit

--- workshop/setup.d/resources-setup.sh
+++ workshop/setup.d/resources-setup.sh
@@ -4,4 +4,3 @@ set +e
 
 kubectl patch serviceaccount default -p '{"secrets": [{"name": "registry-credentials"},{"name": "git-https"}]}'
 kubectl apply -f ~/samples/scan-policy.yaml
-kubectl apply -f ~/samples/test-pipeline.yaml
