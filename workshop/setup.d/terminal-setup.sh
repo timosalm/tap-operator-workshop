0a5c5c9432b9bf7248538cb7ec9791bad98cbbfb
Timo Salm <tsalm@vmware.com>
2023-04-26 12:26:02 +0200

    Initial commit

--- workshop/setup.d/terminal-setup.sh
+++ workshop/setup.d/terminal-setup.sh
@@ -20,6 +20,5 @@ k() {
     fi
 }
 
-export METADATA_STORE_ACCESS_TOKEN=$(kubectl get secrets -n metadata-store -o jsonpath="{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='metadata-store-read-write-client')].data.token}" | base64 -d)
 tanzu insight config set-target  https://metadata-store.${TAP_INGRESS} --access-token=$METADATA_STORE_ACCESS_TOKEN
 tanzu insight health
