a5a16ab3c03dc28c58dd7567a9c8518ef5b6f73d
Timo Salm <tsalm@vmware.com>
2023-04-26 20:45:56 +0200

    Initial commit

--- workshop/setup.d/resources-setup.sh
+++ workshop/setup.d/resources-setup.sh
@@ -4,5 +4,5 @@ set +e
 
 kubectl annotate namespace ${SESSION_NAMESPACE} secretgen.carvel.dev/excluded-from-wildcard-matching-
 
-kubectl patch serviceaccount default -p '{"secrets": [{"name": "registry-credentials"},{"name": "git-https"}]}'
+kubectl patch serviceaccount default -p '{"secrets": [{"name": "registry-credentials"},{"name": "git-https"}], imagePullSecrets: [{"name": "registry-credentials"}]}'
 kubectl apply -f ~/samples/scan-policy.yaml
